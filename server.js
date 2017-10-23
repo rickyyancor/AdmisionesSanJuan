2416b62074a1ebb70e81ccfd1e94d00a2f7c287a
System8 <System8@Info8.local.hospigen.net>
2017-10-23 12:20:49 -0600

    Version 2.0.1 solucionado error espera de base de datos

--- server.js
+++ server.js
@@ -596,92 +596,51 @@ var clientIp = socket.request.connection.remoteAddress;
 var cliente=clientIp.substring(7, 19);
 var idusuario=cliente;
 
+request.on('doneProc', function (rowCount, more, rows) {
 
-
-
-workbook.xlsx.readFile(__dirname + '/reportes/Libro2.xlsx')
-    .then(function() {
-        var worksheet = workbook.getWorksheet(1);
-        var row = worksheet.getRow(6);
-        row.getCell(1).value = data.apellido1+"  "+data.apellido2+"  "+data.nombre1+"  "+data.nombre2; // A5's value set to 5
-        row.getCell(7).value=data.no_expediente;
-        row.commit();
-        row=worksheet.getRow(9);
-        row.getCell(1).value = data.fecha_nac; // A5's value set to 5
-        row.getCell(4).value=data.edad;
-        row.getCell(5).value=data.lugar_nac;
-        row.getCell(7).value=data.sexo;
-        row.commit();
-        row=worksheet.getRow(11);
-        row.getCell(1).value = data.estado_civil; // A5's value set to 5
-        row.getCell(3).value=data.ocupacion,
-        row.getCell(5).value=data.nacionalidad;
-        row.getCell(7).value=data.dpi;
-        row.commit();
-        row=worksheet.getRow(13);
-        row.getCell(1).value = data.contacto_emergencia; // A5's value set to 5
-        row.getCell(4).value=data.parentesco,
-        row.getCell(5).value=data.direccion_contacto;
-        row.getCell(7).value=data.telefono_contacto;
-        row.commit();
-        row=worksheet.getRow(15);
-        row.getCell(1).value = data.fecha_asistencia; // A5's value set to 5
-        row.commit();
-        row=worksheet.getRow(14);
-        row.getCell(4).value = "Hora: "+data.hora;
-        row.getCell(5).value="Area de urgencia: "+data.area_urgencia;
-        row.commit();
-        row=worksheet.getRow(16);
-        row.getCell(4).value = data.tipo_consulta;
-        row.commit();
-        row=worksheet.getRow(19);
-        row.getCell(1).value = data.motivo_consulta;
-        row.commit();
-        row=worksheet.getRow(22);
-        row.getCell(1).value = data.historia_enfermedad;
-        row.commit();
-        row=worksheet.getRow(25);
-        row.getCell(1).value = data.examen_fisico;
-
-
-
-
-
-
-
-
-        return workbook.xlsx.writeFile(__dirname + '/reportes/'+ idusuario+".xlsx");
-      }).then(function(){
-
-
-        msopdf(null, function(error, office) {
-
-            if (error) {
-              console.log("Init failed", error);
-
-            }
-
-           office.excel({input:__dirname + '/reportes/'+ idusuario+".xlsx", output:__dirname + '/reportes/'+ idusuario+".pdf"}, function(error, pdf) {
-               if (error) {
-                   console.log("Woops", error);
-               } else {
-                   console.log("Saved to", pdf);
-
-
-
-               }
-           });
-         office.close(null, function(error) {
-               if (error) {
-                   console.log("Woops", error);
-               } else {
-                   console.log("Finished & closed");
-                   socket.emit('limpiar',idusuario);
-
-               }
-           });
-
-        })
+  workbook.xlsx.readFile(__dirname + '/reportes/Libro2.xlsx')
+      .then(function() {
+          var worksheet = workbook.getWorksheet(1);
+          var row = worksheet.getRow(6);
+          row.getCell(1).value = data.apellido1+"  "+data.apellido2+"  "+data.nombre1+"  "+data.nombre2; // A5's value set to 5
+          row.getCell(7).value=data.no_expediente;
+          row.commit();
+          row=worksheet.getRow(9);
+          row.getCell(1).value = data.fecha_nac; // A5's value set to 5
+          row.getCell(4).value=data.edad;
+          row.getCell(5).value=data.lugar_nac;
+          row.getCell(7).value=data.sexo;
+          row.commit();
+          row=worksheet.getRow(11);
+          row.getCell(1).value = data.estado_civil; // A5's value set to 5
+          row.getCell(3).value=data.ocupacion,
+          row.getCell(5).value=data.nacionalidad;
+          row.getCell(7).value=data.dpi;
+          row.commit();
+          row=worksheet.getRow(13);
+          row.getCell(1).value = data.contacto_emergencia; // A5's value set to 5
+          row.getCell(4).value=data.parentesco,
+          row.getCell(5).value=data.direccion_contacto;
+          row.getCell(7).value=data.telefono_contacto;
+          row.commit();
+          row=worksheet.getRow(15);
+          row.getCell(1).value = data.fecha_asistencia; // A5's value set to 5
+          row.commit();
+          row=worksheet.getRow(14);
+          row.getCell(4).value = "Hora: "+data.hora;
+          row.getCell(5).value="Area de urgencia: "+data.area_urgencia;
+          row.commit();
+          row=worksheet.getRow(16);
+          row.getCell(4).value = data.tipo_consulta;
+          row.commit();
+          row=worksheet.getRow(19);
+          row.getCell(1).value = data.motivo_consulta;
+          row.commit();
+          row=worksheet.getRow(22);
+          row.getCell(1).value = data.historia_enfermedad;
+          row.commit();
+          row=worksheet.getRow(25);
+          row.getCell(1).value = data.examen_fisico;
 
 
 
@@ -690,7 +649,51 @@ workbook.xlsx.readFile(__dirname + '/reportes/Libro2.xlsx')
 
 
 
-    });
+          return workbook.xlsx.writeFile(__dirname + '/reportes/'+ idusuario+".xlsx");
+        }).then(function(){
+
+
+          msopdf(null, function(error, office) {
+
+              if (error) {
+                console.log("Init failed", error);
+
+              }
+
+             office.excel({input:__dirname + '/reportes/'+ idusuario+".xlsx", output:__dirname + '/reportes/'+ idusuario+".pdf"}, function(error, pdf) {
+                 if (error) {
+                     console.log("Woops", error);
+                 } else {
+                     console.log("Saved to", pdf);
+
+
+
+                 }
+             });
+           office.close(null, function(error) {
+                 if (error) {
+                     console.log("Woops", error);
+                 } else {
+                     console.log("Finished & closed");
+                     socket.emit('limpiar',idusuario);
+
+                 }
+             });
+
+          })
+
+
+
+
+
+});
+
+
+
+});
+
+
+
 
 
 
@@ -771,102 +774,67 @@ console.log("nuevo registro ingresos");
   var clientIp = socket.request.connection.remoteAddress;
   var cliente=clientIp.substring(7, 19);
   var idusuario=cliente;
-  workbook.xlsx.readFile('reportes/Libro1.xlsx')
-      .then(function() {
-          var worksheet = workbook.getWorksheet(1);
-          var row = worksheet.getRow(6);
-          row.getCell(1).value = data.n1; // A5's value set to 5
-          row.getCell(3).value=data.n2;
-          row.getCell(5).value=data.n3;
-          row.getCell(7).value=data.n4;
-          row.getCell(9).value=data.n5;
 
-          row.commit();
-          row=worksheet.getRow(8);
-          row.getCell(1).value = data.n6; // A5's value set to 5
-          row.getCell(4).value=data.n7,
-          row.getCell(6).value=data.n8;
-          row.getCell(8).value=data.n9;
-          row.getCell(10).value=data.n10;
-          row.commit();
-          row=worksheet.getRow(10);
-          row.getCell(1).value = data.n11; // A5's value set to 5
-          row.getCell(4).value=data.n12,
-          row.getCell(6).value=data.n13;
-          row.getCell(8).value=data.n14;
-          row.getCell(10).value=data.n15;
-          row.commit();
-          row=worksheet.getRow(12);
-          row.getCell(1).value = data.n16; // A5's value set to 5
-          row.getCell(6).value=data.n17,
-          row.getCell(8).value=data.n18;
-          row.getCell(10).value=data.n19;
-          row.commit();
-          row=worksheet.getRow(14);
-          row.getCell(1).value = data.n20; // A5's value set to 5
-          row.getCell(4).value=data.n21,
-          row.getCell(6).value=data.n22;
-          row.getCell(8).value=data.n23;
-          row.commit();
-          row=worksheet.getRow(16);
-          row.getCell(1).value = data.n24;
-          row.getCell(6).value=data.n25;
-          row.commit();
-          row=worksheet.getRow(18);
-          row.getCell(1).value = data.n26;
-          row.getCell(6).value=data.n27;
-          row.commit();
-          row=worksheet.getRow(20);
-          row.getCell(1).value = data.n28;
-          row.getCell(6).value=data.n29;
-          row.getCell(8).value=data.n30;
-          row.getCell(10).value=data.n31;
-          row.commit();
-          row=worksheet.getRow(22);
-          row.getCell(1).value = data.n32;
-          row.getCell(6).value=data.n33;
-          row.commit();
-          row=worksheet.getRow(24);
-          row.getCell(1).value = data.n34; // A5's value set to 5
-          row.getCell(3).value=data.n35,
-          row.getCell(4).value=data.n36;
+  request.on('doneProc', function (rowCount, more, rows) {
+    workbook.xlsx.readFile('reportes/Libro1.xlsx')
+        .then(function() {
+            var worksheet = workbook.getWorksheet(1);
+            var row = worksheet.getRow(6);
+            row.getCell(1).value = data.n1; // A5's value set to 5
+            row.getCell(3).value=data.n2;
+            row.getCell(5).value=data.n3;
+            row.getCell(7).value=data.n4;
+            row.getCell(9).value=data.n5;
 
-
-
-
-
-
-
-
-
-          return workbook.xlsx.writeFile(__dirname + '/reportes/I'+ idusuario+".xlsx");
-        }).then(function(){
-
-
-          msopdf(null, function(error, office) {
-
-              if (error) {
-                console.log("Init failed", error);
-                return;
-              }
-
-             office.excel({input: __dirname + '/reportes/I'+ idusuario+".xlsx", output: __dirname + '/reportes/I'+ idusuario+".pdf"}, function(error, pdf) {
-                 if (error) {
-                     console.log("Woops", error);
-                 } else {
-                     console.log("Saved to", pdf);
-                     //socket.emit("limpiar");
-                 }
-             });
-           office.close(null, function(error) {
-                 if (error) {
-                     console.log("Woops", error);
-                 } else {
-                     console.log("Finished & closed");
-                     socket.emit('limpiar',idusuario);
-                 }
-             });
-          })
+            row.commit();
+            row=worksheet.getRow(8);
+            row.getCell(1).value = data.n6; // A5's value set to 5
+            row.getCell(4).value=data.n7,
+            row.getCell(6).value=data.n8;
+            row.getCell(8).value=data.n9;
+            row.getCell(10).value=data.n10;
+            row.commit();
+            row=worksheet.getRow(10);
+            row.getCell(1).value = data.n11; // A5's value set to 5
+            row.getCell(4).value=data.n12,
+            row.getCell(6).value=data.n13;
+            row.getCell(8).value=data.n14;
+            row.getCell(10).value=data.n15;
+            row.commit();
+            row=worksheet.getRow(12);
+            row.getCell(1).value = data.n16; // A5's value set to 5
+            row.getCell(6).value=data.n17,
+            row.getCell(8).value=data.n18;
+            row.getCell(10).value=data.n19;
+            row.commit();
+            row=worksheet.getRow(14);
+            row.getCell(1).value = data.n20; // A5's value set to 5
+            row.getCell(4).value=data.n21,
+            row.getCell(6).value=data.n22;
+            row.getCell(8).value=data.n23;
+            row.commit();
+            row=worksheet.getRow(16);
+            row.getCell(1).value = data.n24;
+            row.getCell(6).value=data.n25;
+            row.commit();
+            row=worksheet.getRow(18);
+            row.getCell(1).value = data.n26;
+            row.getCell(6).value=data.n27;
+            row.commit();
+            row=worksheet.getRow(20);
+            row.getCell(1).value = data.n28;
+            row.getCell(6).value=data.n29;
+            row.getCell(8).value=data.n30;
+            row.getCell(10).value=data.n31;
+            row.commit();
+            row=worksheet.getRow(22);
+            row.getCell(1).value = data.n32;
+            row.getCell(6).value=data.n33;
+            row.commit();
+            row=worksheet.getRow(24);
+            row.getCell(1).value = data.n34; // A5's value set to 5
+            row.getCell(3).value=data.n35,
+            row.getCell(4).value=data.n36;
 
 
 
@@ -875,7 +843,50 @@ console.log("nuevo registro ingresos");
 
 
 
-      })
+
+            return workbook.xlsx.writeFile(__dirname + '/reportes/I'+ idusuario+".xlsx");
+          }).then(function(){
+
+
+            msopdf(null, function(error, office) {
+
+                if (error) {
+                  console.log("Init failed", error);
+                  return;
+                }
+
+               office.excel({input: __dirname + '/reportes/I'+ idusuario+".xlsx", output: __dirname + '/reportes/I'+ idusuario+".pdf"}, function(error, pdf) {
+                   if (error) {
+                       console.log("Woops", error);
+                   } else {
+                       console.log("Saved to", pdf);
+                       //socket.emit("limpiar");
+                   }
+               });
+             office.close(null, function(error) {
+                   if (error) {
+                       console.log("Woops", error);
+                   } else {
+                       console.log("Finished & closed");
+                       socket.emit('limpiar',idusuario);
+                   }
+               });
+            })
+
+
+
+
+
+
+
+
+        });
+  });
+
+
+
+
+
 
 
 });
